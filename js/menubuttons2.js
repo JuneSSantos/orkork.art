699b7a5e75d180d6e3151475b4f865f3f8d9648f
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2022-12-19 02:31:14 -0300

    Add files via upload

---
+++ js/menubuttons2.js
@@ -0,0 +1,13 @@
+let headerContent = `
+<a href="../main.html">
+                <img src="../images/buttons/homebutton.png" class="button"></a>
+<a href="../gallery/gallerymain.html">
+                <img src="../images/buttons/gallerybutton.png" class="button"></a>
+<a href="../blog/blogmain.html">
+                <img src="../images/buttons/blogbutton.png" class="button"></a>
+<a href="../comissions/comissionsmain.html">
+                <img src="../images/buttons/comissionsbutton.png" class="button"></a>
+<a href="../guestbook/guestbook.html">
+                <img src="../images/buttons/guestbookbutton.png" class="button"></a>
+`;
+document.querySelector('#buttoncontainer').insertAdjacentHTML('beforeend', headerContent);
